487a03941e8d820814f0e32ce9f7fba9004fc85c
wing787 <42018587+wing787@users.noreply.github.com>
2023-07-19 11:13:14 +0900

    add parameters

--- test_pmtiles/main.js
+++ test_pmtiles/main.js
@@ -3,6 +3,10 @@ import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import * as pmtiles from 'pmtiles';
 
+// parameters
+let polygons_outline_width = 2.5;
+let polygons_outline_color = 'rgba(0, 0, 205, 1)';
+
 const protocol = new pmtiles.Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
@@ -26,10 +30,15 @@ const map = new maplibregl.Map({
         maxzoom: 19,
         attribution: '&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">地理院タイル</a>',
       },
-      pmtiles :{
+      pmtiles_town :{
         type: 'vector',
         url: 'pmtiles://https://test-pmtiles.s3.ap-northeast-1.amazonaws.com/jstat-district/chochomoku_all_fix.pmtiles',
         attribution: '&copy; <a href="https://www.e-stat.go.jp/gis/statmap-search?page=1&type=2&aggregateUnitForBoundary=A&toukeiCode=00200521" target="_blank">2020年国勢調査小地域（町丁・字等別）の境界データを加工し作成</a>',
+      },
+      pmtiles_city :{
+        type: 'vector',
+        url: 'pmtiles://https://test-pmtiles.s3.ap-northeast-1.amazonaws.com/kokudosuuchi/city_master.pmtiles',
+        attribution: '&copy; <a href="https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03-v3_1.html" target="_blank">国土数値情報 - 行政区域データ - </a>',
       }
     },
     layers: [
@@ -39,16 +48,27 @@ const map = new maplibregl.Map({
         type: 'raster',
       },
       {
-        id: 'pmtiles-layer',
-        source: 'pmtiles',
+        id: 'pmtiles_town-layer',
+        source: 'pmtiles_town',
         'source-layer': 'chochomoku_allgeojsonl',
         type: 'line',
-        minzoom: 12,
+        minzoom: 12.5,
         paint: {
-          'line-color': 'rgba(0, 100, 0, 1)',
-          'line-width': 2
+          'line-color': polygons_outline_color,
+          'line-width': polygons_outline_width
         },
       },
+      {
+        id: 'pmtiles_city-layer',
+        source: 'pmtiles_city',
+        'source-layer': 'city_mastergeojsonl',
+        type: 'line',
+        maxzoom: 12.5,
+        paint: {
+          'line-color': polygons_outline_color,
+          'line-width': polygons_outline_width
+        }
+      }
     ]
   },
 });
